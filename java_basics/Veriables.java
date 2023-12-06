900fce2ce1773a6f73db03c40a894f5f724ffe6f
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-12-06 23:51:22 +0530

    veriable done

---
+++ java_basics/Veriables.java
@@ -0,0 +1,20 @@
+package java_basics;
+
+public class Veriables {
+	public static void main(String[] args) {
+		int accountId = 1414;
+		String accountEmail = "demo@gmail.com";
+		String accountPassword = "5454";
+		String accountCity = "kolkata";
+		String accountState;
+		
+		accountEmail = "demo.google@gmail.com";
+        accountPassword = "1234";
+        accountCity = "bangalore";
+		
+		System.out.println("Account ID :" + accountId);
+		System.out.println("Account Email :" + accountEmail);
+		System.out.println("Account Password :" + accountPassword);
+		System.out.println("Account City :" + accountCity);
+	}
+}
