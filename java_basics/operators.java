3efcc140e37862093140c20e6e039dd01bc3c5bb
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-12-08 23:55:27 +0530

    operators done

---
+++ java_basics/operators.java
@@ -0,0 +1,9 @@
+public class operators{
+    public static void main(String[] args){
+        int num1 = 20;
+        double num2 = 20.50;
+        double totalNum = num1 + num2;
+
+        System.out.println("Num1 is " + num1 + " and Num2 is " + num2 + " and total num is :" +totalNum)
+    }
+}
