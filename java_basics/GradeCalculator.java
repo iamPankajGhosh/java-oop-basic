5c455fa56fe28f01797244160d75bde2b720acfb
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-12-11 23:52:36 +0530

    grade calculator done

---
+++ java_basics/GradeCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the percentage: ");
+        double percentage = scanner.nextDouble();
+
+        char grade;
+
+        switch ((int) percentage / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            case 6:
+                grade = 'D';
+                break;
+            default:
+                grade = 'F';
+        }
+
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
