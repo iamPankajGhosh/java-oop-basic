66053fd45080d1f2843b0cd00e3a46fa3d48b0ba
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-12-15 23:24:18 +0530

    ebook added

--- java_basics/LibrarySystem/src/com/library/model/Ebook.java
+++ java_basics/LibrarySystem/src/com/library/model/Ebook.java
@@ -1,1 +1,29 @@
 package com.library.model;
+
+public class Ebook extends Book{
+	private String format;
+	
+	public Ebook(String title, Author author, int pageCount, String format) {
+		super(title, author, pageCount);
+		this.format = format;
+	}
+	
+	@Override
+	public void displayDetails() {
+		System.out.println("Ebook Details:");
+		System.out.println("Title: " + getTitle());
+		System.out.println("Author: " + getAuthor());
+		System.out.println("Page Count: " + getPageCount());
+		System.out.println("Format: " + format);
+	}
+
+	
+	public String toString() {
+		return "Book{" + 
+				"title = '" + getTitle() + '\'' +
+				", author = " + getAuthor() +
+				", pageCount = " + getPageCount() +
+				", format = " + format +
+				'}';
+	}
+}
